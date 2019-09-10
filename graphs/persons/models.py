79dff748270ed6bb584893286f984d3e4b135c3b
aswin <aswincsekar@gmail.com>
2019-09-10 10:45:59 +0530

    serializers and views for new models

--- graphs/persons/models.py
+++ graphs/persons/models.py
@@ -1,17 +1,27 @@
 from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
-    UniqueIdProperty, RelationshipTo)
+    UniqueIdProperty, RelationshipTo, RelationshipFrom, StructuredRel, DateTimeProperty)
+import datetime
+import pytz
 
 config.DATABASE_URL = 'bolt://neo4j:adminpass@localhost:7687'
 
 
 class Country(StructuredNode):
     code = StringProperty(unique_index=True, required=True)
+    inhabitant = RelationshipFrom('Person', 'PERSONS')
+
+
+class FriendRel(StructuredRel):
+    since = DateTimeProperty(
+        default=lambda: datetime.datetime.now(pytz.utc)
+    )
+    met = StringProperty()
+    label = StringProperty(default='')
 
 
 class Person(StructuredNode):
     uid = UniqueIdProperty()
     name = StringProperty(unique_index=True)
     age = IntegerProperty(index=True, default=0)
-
-    # traverse outgoing IS_FROM relations, inflate to Country objects
+    friends = RelationshipTo('Person', 'FRIEND', model=FriendRel)
     country = RelationshipTo(Country, 'IS_FROM')
