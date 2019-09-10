11c8620b26e784bf16589f795e263bdb4ce2154c
aswincsekar <aswincsekar@gmail.com>
2019-09-10 17:28:01 +0530

    new data

--- graphs/persons/serializers.py
+++ graphs/persons/serializers.py
@@ -1,6 +1,2 @@
 from rest_framework import serializers
 
-
-class PersonSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    age = serializers.IntegerField()
--- graphs/persons/views.py
+++ graphs/persons/views.py
@@ -1,2 +1,25 @@
 # path = graph.run(
 #     "MATCH (b1:Person {name: 'Jim'}),(b2:Person {name: 'superman'}), p = shortestPath((b1)-[*..15]-(b2)) RETURN p").to_ndarray()
+
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .utils import StructuredThingSerializer
+from .models import Person
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+
+class PersonViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    def list(self, request):
+        queryset = Person.nodes.all()
+        serializer = StructuredThingSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Person.nodes.all()
+        person = Person.nodes.get(uid=pk)
+        serializer = StructuredThingSerializer(person)
+        return Response(serializer.data)
